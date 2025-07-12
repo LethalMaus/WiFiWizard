083cf547390254664af1f143d1b054f5c4a9ac25
LethalMaus <descriptiveanimals@gmail.com>
2025-07-13 01:17:54 +0200

    Fixed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.jamescullimore.wifiwizard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
