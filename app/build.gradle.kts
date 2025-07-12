21c20ff90fcec97fa8ff0823adf9ceb9467be883
LethalMaus <descriptiveanimals@gmail.com>
2025-07-13 01:09:04 +0200

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.jamescullimore.wifiwizard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
